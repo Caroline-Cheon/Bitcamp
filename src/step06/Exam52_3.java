b5e200dd9e45ee9f7e698c0c6f9ee74c9208aed4
JEEYEON CHEON <JEEYEON CHEON>
2016-10-23 00:15:42 +0900

    d

--- src/step06/Exam52_3.java
+++ src/step06/Exam52_3.java
@@ -6,38 +6,38 @@ package step06;
 
 public class Exam52_3 {
   static void printCenteredStarsLine(int lineSize, int starLength) {
-    if (starLength % 2 == 0) {
-      starLength++;
-    }
     if (lineSize % 2 == 0) {
       lineSize++;
     }
-    int indentLength = (lineSize - starLength) / 2;
-    int starCount = 0;
-    int indentCount = 0;
-    if (indentLength > 0) {
-      while (indentCount++ < indentLength) {
-        System.out.print(" ");
-      }
-      while (starCount++ < starLength) {
-        System.out.print("*");
-      }
-      System.out.println();
+    if (starLength > lineSize) {
+      starLength = lineSize;
     }
+    printMargin(getMarginSize(lineSize, starLength));
+    printStarsLine(starLength);
+
   }
-  static void printBlanks(int length) {
+  static void printStarsLine(int length) {
+    int count = 0;
+    while (count++ < length) {
+      System.out.print("*");
+    }
+    System.out.println();
+  }
+  static int getMarginSize(int lineSize, int starLength) {
+    return (lineSize - starLength) / 2;
+  }
+  static void printMargin(int length) {
     for (int i = 0; i <length; i++) {
       System.out.print(" ");
     }
   }
-  public static void main(String[] args) {
-    int a = Integer.parseInt(args[0]);
-    int count = 1;
-    while (count <= a) {
-      printCenteredStarsLine(a, count);
-      count += 2;
-    }
 
+  public static void main(String[] args) {
+    int starLength = 1;
+    while (starLength <=9) {
+      printCenteredStarsLine(9, starLength);
+      starLength += 2;
+    }
   }
 
 }
--- src/step06/Testing.java
+++ src/step06/Testing.java
@@ -1,46 +1,18 @@
-// 주제: 명령어 묶기 - 메서드 활용 예 (메서드 사용 후) III
-
+// call by reference      값을 가지고 있는 주소를 넘기는 것이다.
 package step06;
 
 public class Testing {
-  // 1) 메서드 정의 : 명령어들을 분리하여 놓고 이름을 부여한 것.
-  static void printStarsLine(int length) {
-    int count = 0;
-    while (count++ < length) {
-      System.out.print("*");
-    }
-    System.out.println();
-  }
-
-  static void printCenterStarsLine(int lineSize, int starLength) {
-    if (lineSize % 2 == 0) {
-      lineSize += 1;
-    }
-    if (starLength > lineSize) {
-      starLength = lineSize;
-    }
-    printBlanks(getBlankSize(lineSize, starLength));
-    printStarsLine(starLength);
-  }
-
-  static int getBlankSize(int lineSize, int starLength) {
-    return (lineSize - starLength) / 2;
-  }
-
-  static void printBlanks(int length) {
-    for (int i = 0; i < length; i++) {
-      System.out.print(" ");
-    }
-  }
-
   public static void main(String[] args) {
-    int count = 6;
-    while (--count > 0) {
-      printStarsLine(count);
+
+    String s1 = "Belly";
+    String s2 = "Belly";
+    String s3 = "Belly";
+
+    if (s1 == s2) {
+      System.out.println("s1 == s2");
     }
-    System.out.println("-------------------------");
-    for (int i = 1; i <= 9; i += 2) {
-      printCenterStarsLine(9, i);
+    if (s1 == s3) {
+      System.out.println("s1 == s3");
     }
   }
 }
